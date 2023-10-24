731734a1ac6ae6ac612300498cfa848ba7263778
Ivan K <ivan.karpovich.frontend@gmail.com>
2023-10-24 08:07:49 +0300

    feat: add base project config

---
+++ .eslintrc.cjs
@@ -0,0 +1,57 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+    node: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb-typescript/base',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'prettier',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  root: true,
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['**/tsconfig.json'],
+  },
+  plugins: ['@typescript-eslint', 'react', 'prettier', 'react-hooks'],
+  rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'comma-dangle': ['error', 'only-multiline'],
+    'react/prop-types': 'off',
+    'react/display-name': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/ban-ts-comment': 'error',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-var-requires': 'off',
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+  },
+  ignorePatterns: ['vite.config.ts'],
+};
---
+++ .prettierrc.cjs
@@ -0,0 +1,11 @@
+const config = {
+    endOfLine: "auto",
+    semi: true,
+    singleQuote: true,
+    tabWidth: 2,
+    trailingComma: "es5",
+    printWidth: 100,
+    arrowParens: "always"
+};
+  
+module.exports = config;
